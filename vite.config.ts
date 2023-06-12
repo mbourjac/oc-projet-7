7b3382f5e6b3fc9377a36b4e4e93c54345b8593d
mbourjac2 <michael.bourjac@gmail.com>
2023-06-12 19:34:24 +0200

    Install and configure postcss preset env

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import postcssPresetEnv from 'postcss-preset-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,11 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+  },
+  css: {
+    devSourcemap: true,
+    postcss: {
+      plugins: [postcssPresetEnv({ stage: 1 })],
+    },
   },
 });
