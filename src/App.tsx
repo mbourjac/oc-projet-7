641fd5231e4b37df2c925e13501897c67df63e2e
mbourjac2 <michael.bourjac@gmail.com>
2023-06-12 19:27:30 +0200

    Install and setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<h1>Hello World</h1>} />)
+);
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
