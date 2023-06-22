cb38c6e66154b496cd68f81fa0788883427a3f5a
mbourjac2 <michael.bourjac@gmail.com>
2023-06-22 20:58:16 +0200

    Create Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import styles from './Card.module.scss';
+
+type CardProps = {
+  id: string;
+  cover: string;
+  title: string;
+};
+
+export const Card = ({ id, cover, title }: CardProps) => {
+  return (
+    <Link to={`/rooms/${id}`}>
+      <article className={styles.card}>
+        <img src={cover} alt="" className={styles.cover} />
+        <h2 className={styles.title}>{title}</h2>
+      </article>
+    </Link>
+  );
+};
