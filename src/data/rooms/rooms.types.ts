8d41b3c7e8e67171814691d75a7588dd59b70541
mbourjac2 <michael.bourjac@gmail.com>
2023-06-23 16:45:34 +0200

    Create Room interface

---
+++ src/data/rooms/rooms.types.ts
@@ -0,0 +1,15 @@
+export interface Room {
+  id: string;
+  title: string;
+  cover: string;
+  pictures: string[];
+  description: string;
+  host: {
+    name: string;
+    picture: string;
+  };
+  rating: string;
+  location: string;
+  equipments: string[];
+  tags: string[];
+}
