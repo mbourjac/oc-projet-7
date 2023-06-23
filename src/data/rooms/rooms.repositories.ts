6baa8f60a6beb94cf639fcc96886917c48515ebc
mbourjac2 <michael.bourjac@gmail.com>
2023-06-23 17:11:57 +0200

    Create JsonRoomsRepository implementation of RoomsRepository

--- src/data/rooms/rooms.repositories.ts
+++ src/data/rooms/rooms.repositories.ts
@@ -4,3 +4,15 @@ export interface RoomsRepository {
   getRoom(id: string): Promise<Room | null>;
   getRooms(): Promise<Room[]>;
 }
+
+export class JsonRoomsRepository implements RoomsRepository {
+  constructor(private rooms: Room[]) {}
+
+  async getRoom(id: string): Promise<Room | null> {
+    return this.rooms.find((room) => room.id === id) ?? null;
+  }
+
+  async getRooms(): Promise<Room[]> {
+    return this.rooms;
+  }
+}
