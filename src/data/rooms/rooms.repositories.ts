0a32447a47d3971a48c6a433bc7f3ec44d5455a8
mbourjac2 <michael.bourjac@gmail.com>
2023-06-24 16:52:52 +0200

    Add delay parameter and withDelay method to JsonRoomsRepository to test long loadings

--- src/data/rooms/rooms.repositories.ts
+++ src/data/rooms/rooms.repositories.ts
@@ -6,13 +6,19 @@ export interface RoomsRepository {
 }
 
 export class JsonRoomsRepository implements RoomsRepository {
-  constructor(private rooms: IRoom[]) {}
+  constructor(private rooms: IRoom[], private delay: number = 0) {}
 
   async getRoom(id: string | undefined): Promise<IRoom | null> {
+    await this.withDelay();
     return this.rooms.find((room) => room.id === id) ?? null;
   }
 
   async getRooms(): Promise<IRoom[]> {
+    await this.withDelay();
     return this.rooms;
+  }
+
+  private withDelay(): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, this.delay));
   }
 }
