b0890c8e816595014bae260d41e8340b8dd1dccb
mbourjac2 <michael.bourjac@gmail.com>
2023-06-24 10:14:10 +0200

    Reorganize Router file

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,16 +6,16 @@ import {
 } from 'react-router-dom';
 import { AppLayout } from './components/AppLayout/AppLayout';
 import { Home, loader as homeLoader } from './pages/Home/Home';
+import { Room } from './pages/Room/Room';
 import { About } from './pages/About/About';
 import { NotFound } from './pages/NotFound/NotFound';
-import { Room } from './pages/Room/Room';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
       <Route index element={<Home />} loader={homeLoader} />
-      <Route path="about" element={<About />} />
       <Route path="rooms/:id" element={<Room />} />
+      <Route path="about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
