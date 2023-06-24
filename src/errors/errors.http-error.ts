b5c4a1d9a48f54751e2b77b7b6b3084747734f08
mbourjac2 <michael.bourjac@gmail.com>
2023-06-24 15:49:33 +0200

    Create HttpError class

---
+++ src/errors/errors.http-error.ts
@@ -0,0 +1,16 @@
+import { HttpErrorResponse } from './errors.types';
+
+export class HttpError extends Error {
+  constructor(public message: string, public status: number) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+
+  toJSON(): HttpErrorResponse {
+    return {
+      name: this.name,
+      status: this.status,
+      message: this.message,
+    };
+  }
+}
