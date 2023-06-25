9928541e765f69f26abb17946397762f63d778a6
mbourjac2 <michael.bourjac@gmail.com>
2023-06-25 06:38:45 +0200

    Defer the loader data of the Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
-import { useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { defer, useLoaderData, Await } from 'react-router-dom';
 import { Banner } from '../../components/Banner/Banner';
 import { Card } from '../../components/Card/Card';
 import { IRoom } from '../../data/rooms/rooms.types';
@@ -9,14 +10,18 @@ import bannerImageS from '@images/home-banner-s.jpg';
 import bannerImageM from '@images/home-banner-m.jpg';
 import bannerImageL from '@images/home-banner-l.jpg';
 
-export const loader = async (): Promise<IRoom[]> => {
+type LoaderData = {
+  rooms: Promise<IRoom[]>;
+};
+
+export const loader = async () => {
   const roomsRepository = new JsonRoomsRepository(roomsJson);
 
-  return roomsRepository.getRooms();
+  return defer({ rooms: roomsRepository.getRooms() });
 };
 
 export const Home = () => {
-  const rooms = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const loaderData = useLoaderData() as LoaderData;
   const bannerImage = {
     small: bannerImageS,
     medium: bannerImageM,
@@ -32,9 +37,19 @@ export const Home = () => {
         </h1>
       </Banner>
       <section className={styles.rooms}>
-        {rooms.map(({ id, cover, title }) => (
-          <Card key={id} id={id} cover={cover} title={title} />
-        ))}
+        <Suspense fallback={<h2>Loading rooms...</h2>}>
+          <Await resolve={loaderData.rooms}>
+            {(rooms: Awaited<LoaderData['rooms']>) => {
+              return (
+                <>
+                  {rooms.map(({ id, cover, title }) => (
+                    <Card key={id} id={id} cover={cover} title={title} />
+                  ))}
+                </>
+              );
+            }}
+          </Await>
+        </Suspense>
       </section>
     </>
   );
