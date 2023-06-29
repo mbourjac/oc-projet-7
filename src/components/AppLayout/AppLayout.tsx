17171bd083b9f4c69d2dc5288ee99a3c6fc11d76
mbourjac2 <michael.bourjac@gmail.com>
2023-06-29 08:24:55 +0200

    Add ScrollRestoration component to AppLayout

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
 import styles from './AppLayout.module.scss';
@@ -10,6 +10,7 @@ type AppLayoutProps = {
 export const AppLayout = ({ children }: AppLayoutProps) => {
   return (
     <>
+      <ScrollRestoration />
       <Header />
       <main className={styles.main}>{children ?? <Outlet />}</main>
       <Footer />
