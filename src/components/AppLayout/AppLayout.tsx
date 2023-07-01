59197e334a54a54d5ad016ccde4c514702034ae1
mbourjac2 <michael.bourjac@gmail.com>
2023-07-01 18:30:15 +0200

    Remove ScrollRestoration component

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
 import styles from './AppLayout.module.scss';
@@ -10,7 +10,6 @@ type AppLayoutProps = {
 export const AppLayout = ({ children }: AppLayoutProps) => {
   return (
     <>
-      <ScrollRestoration />
       <Header />
       <main className={styles.main}>{children ?? <Outlet />}</main>
       <Footer />
