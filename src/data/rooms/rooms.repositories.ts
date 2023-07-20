031673b88ffc485c4b2fe9488a73674809299520
mbourjac2 <michael.bourjac@gmail.com>
2023-07-20 13:28:45 +0200

    Add searchQuery parameter to getRooms method

--- src/data/rooms/rooms.repositories.ts
+++ src/data/rooms/rooms.repositories.ts
@@ -2,7 +2,11 @@ import { IRoom, IGetRooms } from './rooms.types';
 
 export interface RoomsRepository {
   getRoom(id: string | undefined): Promise<IRoom | null>;
-  getRooms(page: number, tags?: string[]): Promise<IGetRooms>;
+  getRooms(
+    page: number,
+    tags?: string[],
+    searchQuery?: string
+  ): Promise<IGetRooms>;
   getAllUniqueTags(): Promise<string[]>;
 }
 
@@ -10,7 +14,11 @@ abstract class AbstractRoomsRepository implements RoomsRepository {
   readonly roomsLimit = 9;
 
   abstract getRoom(id: string | undefined): Promise<IRoom | null>;
-  abstract getRooms(page: number, tags?: string[]): Promise<IGetRooms>;
+  abstract getRooms(
+    page: number,
+    tags?: string[],
+    searchQuery?: string
+  ): Promise<IGetRooms>;
   abstract getAllUniqueTags(): Promise<string[]>;
 }
 
@@ -27,18 +35,23 @@ export class JsonRoomsRepository extends AbstractRoomsRepository {
     return this.rooms.find((room) => room.id === id) ?? null;
   }
 
-  async getRooms(page: number, tags?: string[]): Promise<IGetRooms> {
+  async getRooms(
+    page: number,
+    tags?: string[],
+    searchQuery?: string
+  ): Promise<IGetRooms> {
     await this.withDelay();
 
-    const filteredRooms = tags
-      ? this.rooms.filter((room) =>
-          tags.every((tag) =>
-            room.tags.some(
-              (roomTag) => roomTag.toLowerCase() === tag.toLowerCase()
-            )
-          )
-        )
-      : this.rooms;
+    let filteredRooms = this.rooms;
+
+    if (tags && tags.length > 0) {
+      filteredRooms = this.filterRoomsByTags(filteredRooms, tags);
+    }
+
+    if (searchQuery) {
+      filteredRooms = this.filterRoomsBySearchQuery(filteredRooms, searchQuery);
+    }
+
     const startIndex = (page - 1) * this.roomsLimit;
     const endIndex = startIndex + this.roomsLimit;
     const rooms = filteredRooms.slice(startIndex, endIndex);
@@ -64,6 +77,76 @@ export class JsonRoomsRepository extends AbstractRoomsRepository {
     });
 
     return Array.from(tagsSet);
+  }
+
+  private filterRoomsByTags(rooms: IRoom[], tags: string[]): IRoom[] {
+    return rooms.filter((room) =>
+      tags.every((tag) =>
+        room.tags.some((roomTag) => roomTag.toLowerCase() === tag.toLowerCase())
+      )
+    );
+  }
+
+  private filterRoomsBySearchQuery(
+    rooms: IRoom[],
+    searchQuery: string
+  ): IRoom[] {
+    const normalizedString = (string: string) =>
+      string
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/\p{Diacritic}/gu, '');
+
+    const isMatch = (roomTerm: string): boolean => {
+      const normalizedRoomTerm = normalizedString(roomTerm);
+      const normalizedSearchTerms = normalizedString(searchQuery).split(' ');
+
+      return normalizedSearchTerms.some((normalizedSearchTerm) =>
+        this.isSimilarByLevenshtein(normalizedRoomTerm, normalizedSearchTerm)
+      );
+    };
+
+    return rooms.filter((room) => {
+      const roomProperties = [room.title, room.description, room.equipments];
+      const roomTerms = roomProperties
+        .flat()
+        .flatMap((string) => string.split(' '));
+
+      return roomTerms.some((roomTerm) => isMatch(roomTerm));
+    });
+  }
+
+  private isSimilarByLevenshtein(source: string, target: string): boolean {
+    const allowedDistance = 1;
+    const distance = this.calculateLevenshteinDistance(source, target);
+    return distance <= allowedDistance;
+  }
+
+  private calculateLevenshteinDistance(source: string, target: string): number {
+    const sourceLength = source.length;
+    const targetLength = target.length;
+    const distanceMatrix: number[][] = [];
+
+    for (let i = 0; i <= sourceLength; i++) {
+      distanceMatrix[i] = [i];
+    }
+
+    for (let j = 1; j <= targetLength; j++) {
+      distanceMatrix[0][j] = j;
+    }
+
+    for (let i = 1; i <= sourceLength; i++) {
+      for (let j = 1; j <= targetLength; j++) {
+        const cost = source[i - 1] !== target[j - 1] ? 1 : 0;
+        distanceMatrix[i][j] = Math.min(
+          distanceMatrix[i - 1][j] + 1,
+          distanceMatrix[i][j - 1] + 1,
+          distanceMatrix[i - 1][j - 1] + cost
+        );
+      }
+    }
+
+    return distanceMatrix[sourceLength][targetLength];
   }
 
   private withDelay(delay?: number): Promise<void> {
