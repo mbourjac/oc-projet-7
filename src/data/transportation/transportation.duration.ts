ce602243214568d71f490679e680643d57e33fbd
mbourjac2 <michael.bourjac@gmail.com>
2023-08-16 19:12:24 +0200

    Create the Duration class

---
+++ src/data/transportation/transportation.duration.ts
@@ -0,0 +1,34 @@
+export class Duration {
+  constructor(readonly seconds: number) {}
+
+  getFormattedDuration(): string {
+    const { hours, minutes } = this.convertToHoursAndMinutes();
+    const formattedHours = `${hours} heure${hours === 1 ? '' : 's'}`;
+    const formattedMinutes = `${minutes} minute${minutes === 1 ? '' : 's'}`;
+
+    if (hours === 0) {
+      return formattedMinutes;
+    }
+
+    if (minutes === 0) {
+      return formattedHours;
+    }
+
+    return `${formattedHours} et ${formattedMinutes}`;
+  }
+
+  private convertToHoursAndMinutes(): {
+    hours: number;
+    minutes: number;
+  } {
+    if (this.seconds < 60) {
+      return { hours: 0, minutes: 1 };
+    }
+
+    const totalMinutes = Math.round(this.seconds / 60);
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+
+    return { hours, minutes };
+  }
+}
