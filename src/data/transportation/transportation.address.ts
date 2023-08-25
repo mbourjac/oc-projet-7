88b7dbd1f4d65ccb52bd333ba51f4c8c6e8e09d6
mbourjac2 <michael.bourjac@gmail.com>
2023-08-25 15:56:42 +0200

    Change the type of the address argument from string to IAddress and rework the Address class methods

--- src/data/transportation/transportation.address.ts
+++ src/data/transportation/transportation.address.ts
@@ -1,12 +1,34 @@
 import { GeocodingService } from './transportation.geocoding';
+import { IAddress, ICoordinates } from './transportation.types';
 
 export class Address {
+  private _coordinates?: ICoordinates;
+
   constructor(
-    readonly address: string,
+    readonly address: IAddress,
     private readonly geocodingService: GeocodingService
   ) {}
 
-  get details() {
-    return this.geocodingService.getAddressDetails(this.address);
+  get fullAddress() {
+    const { number, street, city, postcode } = this.address;
+
+    return `${number ?? ''}${street} ${city} ${postcode ?? ''}`.trim();
+  }
+
+  get coordinates(): Promise<ICoordinates> {
+    if (this._coordinates) {
+      return Promise.resolve(this._coordinates);
+    }
+
+    return this.fetchCoordinates();
+  }
+
+  private async fetchCoordinates() {
+    const coordinates = await this.geocodingService.getAddressCoordinates(
+      this.fullAddress
+    );
+
+    this._coordinates = coordinates;
+    return this._coordinates;
   }
 }
