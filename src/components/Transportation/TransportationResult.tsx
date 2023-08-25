5eb1f1130e9608443493b45e55394b9cbb7c084d
mbourjac2 <michael.bourjac@gmail.com>
2023-08-25 16:39:36 +0200

    Create TransportationResult component

---
+++ src/components/Transportation/TransportationResult.tsx
@@ -0,0 +1,37 @@
+import { nanoid } from 'nanoid';
+import { ISearchResult } from '../../data/transportation/transportation.types';
+import styles from './TransportationResult.module.scss';
+
+type TransportationResultProps = {
+  searchResult: ISearchResult | null;
+};
+
+export const TransportationResult = ({
+  searchResult,
+}: TransportationResultProps) => {
+  const isSearchSuccess = searchResult && searchResult.status === 'success';
+  const isSearchError = searchResult && searchResult.status === 'error';
+
+  return (
+    <div className={styles.result}>
+      <p
+        className={`${styles.label} ${
+          isSearchSuccess ? styles.success : ''
+        }`.trim()}
+      >
+        Mode(s) et temps de trajet
+      </p>
+      {isSearchSuccess ? (
+        <ul className={styles.list}>
+          {searchResult.data.map((result) => (
+            <li key={nanoid()} className={styles.item}>
+              {result}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        isSearchError && <p className={styles.error}>{searchResult.message}</p>
+      )}
+    </div>
+  );
+};
